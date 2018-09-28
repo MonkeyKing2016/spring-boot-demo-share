aa041bccea63233f088c6540bb3b9aad25d8b8fa
MonkeyKing <1584747392@qq.com>
2018-09-28 17:44:52 +0800

    修改地址

--- springboot-simple/src/main/java/top/monkeykingshare/demo/springboot/simple/SimpleApplication.java
+++ springboot-simple/src/main/java/top/monkeykingshare/demo/springboot/simple/SimpleApplication.java
@@ -1,4 +1,4 @@
-package com.monkeykingshare.demo.springboot.simple;
+package top.monkeykingshare.demo.springboot.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
